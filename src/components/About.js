2df1e22fd66cdbb7894a7fdcdf3a2e5a112a8f8e
Przemek Tomaszewski <tomaszewski.przemyslaw1993@gmail.com>
2022-12-19 10:02:22 +0100

    clear project + components home,about,contact created

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<div
+			className='ui raised very padded text container segment'
+			style={{ marginTop: "80px" }}>
+			<h3 className='ui header'>About</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta
+				minus molestiae vel beatae natus eveniet.
+			</p>
+		</div>
+	);
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Contact = () => {
+	return (
+		<div
+			className='ui raised very padded text container segment'
+			style={{ marginTop: "80px" }}>
+			<h3 className='ui header'>Contact</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta
+				minus molestiae vel beatae natus eveniet ratione temporibus aperiam
+				harum alias officiis assumenda officia quibusdam deleniti eos cupiditate
+				dolore doloribus!
+			</p>
+		</div>
+	);
+};
+
+export default Contact;
