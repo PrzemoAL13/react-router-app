4aa436a8461645e2b615b1d6f70d49fdd595511d
Przemek Tomaszewski <tomaszewski.przemyslaw1993@gmail.com>
2022-12-19 11:01:17 +0100

    react router imported and navbar links connected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
 	return (
-		<div className='App'>
-			<Navbar></Navbar>
-		</div>
+		<Router>
+			<div className='App'>
+				<Navbar />
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/contact' element={<Contact />} />
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,4 @@
 import React from "react";
-import Home from "./Home";
-import Contact from "./Contact";
-import About from "./About";
 
 const Navbar = () => {
 	return (
