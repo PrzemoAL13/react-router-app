31ef46f4483d41da4ad122f7b14fb13af8ae4f52
Przemek Tomaszewski <tomaszewski.przemyslaw1993@gmail.com>
2022-12-21 18:22:44 +0100

    modal and portal windows

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,21 @@
 import React from "react";
+import Modal from "./modal";
 
 const Contact = () => {
 	return (
-		<div
-			className='ui raised very padded text container segment'
-			style={{ marginTop: "80px" }}>
-			<h3 className='ui header'>Contact</h3>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta
-				minus molestiae vel beatae natus eveniet ratione temporibus aperiam
-				harum alias officiis assumenda officia quibusdam deleniti eos cupiditate
-				dolore doloribus!
-			</p>
+		<div>
+			<Modal />
+			<div
+				className='ui raised very padded text container segment'
+				style={{ marginTop: "80px" }}>
+				<h3 className='ui header'>Contact</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta
+					minus molestiae vel beatae natus eveniet ratione temporibus aperiam
+					harum alias officiis assumenda officia quibusdam deleniti eos
+					cupiditate dolore doloribus!
+				</p>
+			</div>
 		</div>
 	);
 };
